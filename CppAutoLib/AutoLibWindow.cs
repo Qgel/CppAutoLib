a79671ff07a19b6937ad309b934a1c2867789b6f
Stefan Schake <schake@embedded.rwth-aachen.de>
2015-09-30 18:38:50 +0200

    Update results picture
    Signed-off-by: Stefan Schake <schake@embedded.rwth-aachen.de>

--- CppAutoLib/AutoLibWindow.cs
+++ CppAutoLib/AutoLibWindow.cs
@@ -34,7 +34,7 @@ namespace CppAutoLib
         /// </summary>
         public AutoLibWindow() : base(null)
         {
-            this.Caption = "CppAutoLib - inactive";
+            this.Caption = "CppAutoLib";
 
             // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
             // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
